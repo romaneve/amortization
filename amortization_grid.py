4744d05202ffd7521ddc7f231b45c8367cb38d26
romaneve <romaneve@pop-os@mailman.honeyman.life>
2018-12-16 16:28:33 -0700

    circleci testing setup

--- amortization_grid.py
+++ amortization_grid.py
@@ -3,37 +3,39 @@
 # # # # # This is allows you to generate an amortization table
 # /(_)\ # calculated from the principle, interest, loan duration
 # \* */ # and writes it to the file name passed from the command
-#  \_/  # line. This script produces one file and returns a message 
+#  \_/  # line. This script produces one file and returns a message
 # # # # # containing the monthly payment.
 
-import sys
+# import sys
 import argparse as ap
 import os.path as op
-import math
+# import math
 from pandas import DataFrame as xl
 from pandas import ExcelWriter as ew
-import xlsxwriter
+# import xlsxwriter
 
 
-def i_amortize(p,r,n):
+def i_amortize(p, r, n):
     # calculate the monthly interest and apply the
     # amortization formula
     r = r/12
-    a = p*( (r*((1+r)**n))/((1+r)**n-1) )
+    a = p*((r*((1+r)**n))/((1+r)**n-1))
     return(a)
 
-def zero_interest(p,n):
+
+def zero_interest(p, n):
     # this is for calculating the monthly payment
     # for no interest loans
     a = p/n
     return(a)
 
-def i_schedule(p,r,n):
+
+def i_schedule(p, r, n):
     # calculate the monthly interest rate and pass to
     # i_amortize to calculate the monthly payment and
     # calculate the total loan ammount.
     m_interest = r/12
-    payment = i_amortize(p,r,n)
+    payment = i_amortize(p, r, n)
     balance = payment*n
     p_balance = p
 
@@ -44,13 +46,13 @@ def i_schedule(p,r,n):
         i_paid = p_balance*m_interest
         p_paid = payment-i_paid
         t = {
-                "balance": round(balance,2),
-                "principle": round(p_paid,2),
-                "interest": round(i_paid,2),
-                "payment": round(payment,2)
+                "balance": round(balance, 2),
+                "principle": round(p_paid, 2),
+                "interest": round(i_paid, 2),
+                "payment": round(payment, 2)
                 }
         rows.append(t)
-        
+
         # reduce the total loan balance by the payment
         # and reduce the principle balance by the
         # principle paid in this payment
@@ -58,25 +60,27 @@ def i_schedule(p,r,n):
         p_balance = p_balance-p_paid
     return(rows)
 
+
 def o_schedule(rows):
     # stuff the rows in a data frame and re-order the
     # columns for output. I may add some formatting
     # here to allow for usable output to screen
     df = xl(rows)
-    a = df.reindex(columns=['balance','principle','interest','payment'])
+    a = df.reindex(columns=['balance', 'principle', 'interest', 'payment'])
     b = a.applymap("${0:,.2f}".format)
     return(b)
 
-def xlwriter(rows,p,r,n,title):
+
+def xlwriter(rows, p, r, n, title):
     # convert the interest rate to monthly for output.
     # pass p,r,n to i_amortize to retrieve the monthly
     # payment. calculate the total loan after interest.
     mpr = (r/100)/12
-    mp = i_amortize(p,r,n)
+    mp = i_amortize(p, r, n)
     amount = mp*n
     name = title+".xlsx"
     if op.isfile(name):
-        ErrorBack = (1,f"{title}.xlsx already exists")
+        ErrorBack = (1, f"{title}.xlsx already exists")
         return ErrorBack
 
     # stuff the rows from the passed dictionary into a
@@ -87,14 +91,14 @@ def xlwriter(rows,p,r,n,title):
     # attempt to output the data frame to a spreadsheet
     try:
         df1.to_excel(writer, startrow=6, index=False)
-        ErrorBack = (0,'success!')
+        ErrorBack = (0, 'success!')
     except ValueError:
         # more error handling will go here as testing
         # progresses. for now it returns a 1 value for
         # a ValueError
-        ErrorBack = (2,"some other funky thing went wrong")
+        ErrorBack = (2, "some other funky thing went wrong")
         return ErrorBack
-    
+
     # open a workbook and worksheet
     workbook = writer.book
     worksheet = writer.sheets['Sheet1']
@@ -104,10 +108,10 @@ def xlwriter(rows,p,r,n,title):
     format_mpr = workbook.add_format({'num_format': '%.######'})
     format_num = workbook.add_format({'num_format': '#,###'})
     format_cur = workbook.add_format({'num_format': '$#,##0.00'})
-    
+
     # set the format for all columns to currency
     worksheet.set_column('A:E', 18, format_cur)
-    
+
     # write the summary info at the top
     worksheet.write(0, 0, 'Loan Amount')
     worksheet.write(0, 1, p, format_cur)
@@ -119,30 +123,32 @@ def xlwriter(rows,p,r,n,title):
     worksheet.write(3, 1, mpr, format_mpr)
     worksheet.write(4, 0, 'Total Loan Amount')
     worksheet.write(4, 1, amount, format_cur)
-   
+
     # save the spreadsheet
     writer.save()
     return ErrorBack
 
-def launch(pri,rte,trm,title):
+
+def launch(pri, rte, trm, title):
     # this is used to kick off the processing. the
     # arguments are passed into main and then launched
     # from here. this provides the facility to use
     # 'from amortization_grid import launch' to execute
     # from other scripts.
     if rte == 0:
-        o_put = zero_interest(pri,trm)
-        print('%s payments at %s = %s' % (trm, round(o_put,2), o_put*trm))
+        o_put = zero_interest(pri, trm)
+        print('%s payments at %s = %s' % (trm, round(o_put, 2), o_put*trm))
     else:
         pct = rte/100
-        o_put = i_amortize(pri,pct,trm)
-        o = i_schedule(pri,pct,trm)
+        o_put = i_amortize(pri, pct, trm)
+        o = i_schedule(pri, pct, trm)
         q = o_schedule(o)
-        x = xlwriter(q,pri,pct,trm,title)
+        x = xlwriter(q, pri, pct, trm, title)
         if x[0] == 0:
-            print('your monthly payment will be %s\n' % (round(o_put,2)))
+            print('your monthly payment will be %s\n' % (round(o_put, 2)))
         else:
-            print('Error %s: %s\n' % (x[0],x[1]))
+            print('Error %s: %s\n' % (x[0], x[1]))
+
 
 if __name__ == "__main__":
     try:
@@ -150,27 +156,27 @@ if __name__ == "__main__":
         args = ap.ArgumentParser()
         args.add_argument(
                 "--principle",
-                nargs = "*",
-                type = float,
-                default = 5000.00,
+                nargs="*",
+                type=float,
+                default=5000.00,
                 )
         args.add_argument(
                 "--apr",
-                nargs = "*",
-                type = float,
-                default = 5.5,
+                nargs="*",
+                type=float,
+                default=5.5,
                 )
         args.add_argument(
                 "--months",
-                nargs = "*",
-                type = int,
-                default = 36,
+                nargs="*",
+                type=int,
+                default=36,
                 )
         args.add_argument(
                 "--title",
-                nargs = "*",
-                type = str,
-                default = "new_script",
+                nargs="*",
+                type=str,
+                default="new_script",
                 )
         a = args.parse_args()
         pri = a.principle[0]
@@ -178,6 +184,6 @@ if __name__ == "__main__":
         trm = a.months[0]
         title = a.title[0]
         # launch it!
-        launch(pri,rte,trm,title)
+        launch(pri, rte, trm, title)
     except IndexError:
         print('principle, rate, and term are required.')
--- test_amortization_grid.py
+++ test_amortization_grid.py
@@ -2,34 +2,30 @@
 Unit test for amortization_grid.py
 """
 
-import pytest, os
+import os
 import amortization_grid as ag
 
 
 class Test_AG:
 
-    p = 25000.00 # principle
-    r = 4.5/100 # interest rate
-    m = 36 # number of payments
-    c = 743.67 # test check value
-
+    p = 25000.00  # principle
+    r = 4.5/100  # interest rate
+    m = 36  # number of payments
+    c = 743.67  # test check value
 
     def test_i_amortize(self):
         d = ag.i_amortize(self.p, self.r, self.m)
         assert self.c == round(d, 2)
-
 
     def test_i_schedule(self):
         d = ag.i_schedule(self.p, self.r, self.m)
         assert 36 == len(d)
         assert self.c == d[35]['balance']
 
-
     def test_o_schedule(self):
         r = ag.i_schedule(self.p, self.r, self.m)
         d = ag.o_schedule(r)
-        assert self.c == float(d['balance'][35][1:].replace(',',''))
-
+        assert self.c == float(d['balance'][35][1:].replace(',', ''))
 
     def test_xlwriter(self):
         r = ag.i_schedule(self.p, self.r, self.m)
